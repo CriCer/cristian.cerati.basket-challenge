02531f476af8fdae2021d55887a41fba41d45221
CriCer <c.cerati04@gmail.com>
2026-02-19 22:22:55 +0100

    Added cloth net simulation

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class BallHandler : MonoBehaviour
 {
+    const string TriggerTag = "BallTrigger";
+
+    public Vector3 force;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     public void Shoot()
     {
+        rb.AddForce(force, ForceMode.Impulse);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == TriggerTag)
+        {
+            Debug.Log("Green");
+        }
     }
 }
