ceabe6fd6c34248ea908d450b575f7b80cf82164
CriCer <c.cerati04@gmail.com>
2026-02-22 15:16:00 +0100

    Camera ball follow

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public float cameraBackOffset;
+    public float cameraUpOffset;
+    public float cameraSpeed = 1.5f;
+
+    [SerializeField]
+    GameObject ball;
+    [SerializeField]
+    GameObject backgroundObject;
+    [SerializeField]
+    GameObject lookAtObject;
+
+    private void Start()
+    {
+        if (ball == null || backgroundObject == null) return;
+
+        transform.position = GetCameraPosition();
+    }
+
+    private void Update()
+    {
+        if (ball == null || backgroundObject == null) return;
+
+        transform.position = Vector3.Lerp(transform.position, GetCameraPosition(), cameraSpeed * Time.deltaTime);
+
+        if (lookAtObject == null) return;
+        
+        transform.LookAt(lookAtObject.transform.position);
+    }
+
+    Vector3 GetCameraPosition()
+    {
+        Vector3 flatBackgroundObjectPos = backgroundObject.transform.position;
+        flatBackgroundObjectPos.y = 0;
+        Vector3 flatBallPos = ball.transform.position;
+        flatBallPos.y = 0;
+
+        Vector3 direction = (flatBackgroundObjectPos - flatBallPos).normalized;
+
+        Vector3 final = ball.transform.position - direction * cameraBackOffset + Vector3.up * cameraUpOffset;
+
+        return final;
+    }
+}
