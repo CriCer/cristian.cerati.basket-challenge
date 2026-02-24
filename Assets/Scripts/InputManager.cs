e0094cabc5304eb6397515377b6200982939dfcc
CriCer <c.cerati04@gmail.com>
2026-02-24 21:15:19 +0100

    Reworked inputs
    Mixed PC inputs and mobile inputs in input manager due to semplicity for small project

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+using Finger = UnityEngine.InputSystem.EnhancedTouch.Finger;
+using EnhancedTouchSupport = UnityEngine.InputSystem.EnhancedTouch.EnhancedTouchSupport;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    public delegate void OnSwipeStart(Vector2 position);
+    public static event OnSwipeStart onSwipeStart;
+
+    public delegate void OnSwipeEnd(Vector2 startPosition, Vector2 endPosition);
+    public static event OnSwipeEnd onSwipeEnd;
+
+    public delegate void OnSwipeUpdate(Vector2 startPosition, Vector2 currentPosition);
+    public static event OnSwipeUpdate onSwipeUpdate;
+
+    public bool inputEnabled = true;
+
+    [SerializeField]
+    float maxHoldTime = 1.5f;
+    
+
+    List<RaycastResult> uiEventList = new List<RaycastResult>();
+    PointerEventData pointer;
+    Finger currentFinger;
+
+    float holdTimer = 0f;
+    bool holding = false;
+    bool pressStart = false;
+    bool mouseInput = false;
+    Vector2 startPosition;
+
+    void Awake()
+    {
+        pointer = new PointerEventData(EventSystem.current);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3f);
+        inputEnabled = true;
+    }
+
+    void Update()
+    {
+        if (!inputEnabled) return;
+
+        #region MouseInput
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseInput = true;
+            StartSwipe(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            EndSwipe(Input.mousePosition);
+            mouseInput = false;
+        }
+        #endregion
+
+        #region SwipeUpdate
+        if (!pressStart || !holding)
+            return;
+
+        holdTimer += Time.deltaTime;
+
+        if (holdTimer >= maxHoldTime)
+        {
+            EndSwipe(GetCurrentPosition());
+            return;
+        }
+
+        Vector2 current = NormalizeScreenPosition(GetCurrentPosition());
+        onSwipeUpdate?.Invoke(startPosition, current);
+        #endregion
+    }
+
+    public void StartSwipe(Vector2 currentPosition)
+    {
+        if (!inputEnabled) return;
+
+        // Prevent gesture if UI pressed
+        uiEventList.Clear();
+        pointer.position = currentPosition;
+        EventSystem.current.RaycastAll(pointer, uiEventList);
+
+        if (uiEventList.Count > 0)
+            return;
+
+        startPosition = NormalizeScreenPosition(currentPosition);
+
+        pressStart = true;
+        holding = true;
+        holdTimer = 0f;
+
+        onSwipeStart?.Invoke(currentPosition);
+    }
+
+    public void EndSwipe(Vector2 currentPosition)
+    {
+        if (!inputEnabled) return;
+
+        if (!pressStart)
+            return;
+
+        // Prevent gesture if UI pressed
+        uiEventList.Clear();
+        pointer.position = startPosition;
+        EventSystem.current.RaycastAll(pointer, uiEventList);
+
+        if (uiEventList.Count > 0)
+            return;
+
+        holding = false;
+        pressStart = false;
+        holdTimer = 0f;
+
+        currentPosition = NormalizeScreenPosition(currentPosition);
+        onSwipeEnd?.Invoke(startPosition, currentPosition);
+    }
+
+    Vector2 GetCurrentPosition()
+    {
+        if (mouseInput)
+            return Input.mousePosition;
+
+        if (currentFinger != null && currentFinger.currentTouch.valid)
+            return currentFinger.currentTouch.screenPosition;
+
+        return startPosition;
+    }
+
+    #region TouchInput
+
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += TouchFingerDown;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerUp += TouchFingerUp;
+    }
+
+    void OnDisable()
+    {
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= TouchFingerDown;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerUp -= TouchFingerUp;
+        EnhancedTouchSupport.Disable();
+    }
+
+    void TouchFingerDown(Finger finger)
+    {
+        if (finger.index != 0)
+            return;
+
+        mouseInput = false;
+        currentFinger = finger;
+
+        StartSwipe(finger.screenPosition);
+    }
+
+    void TouchFingerUp(Finger finger)
+    {
+        if (finger.index != 0)
+            return;
+
+        EndSwipe(finger.screenPosition);
+        currentFinger = null;
+    }
+    #endregion
+
+    Vector2 NormalizeScreenPosition(Vector2 screenPos)
+    {
+        return new Vector2(
+            screenPos.x / Screen.width,
+            screenPos.y / Screen.height
+        );
+    }
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,19 +7,23 @@ public class PlayerManager : MonoBehaviour
 {
     public BallHandler ball;
     public UIManager uiManager;
-    public float maxPower = 10f;
-    public float minPower = 4f;
-    public float error = 0.05f;
 
     [SerializeField]
+    float maxPower = 10f;
+    [SerializeField]
+    float minPower = 4f;
+    [SerializeField]
+    float error = 0.05f;
+    [SerializeField]
     float maxSwipeDistance = 4f;
+    [SerializeField]
+    float inputMultiplier = 500f;
 
     void Start()
     {
         if (uiManager != null && ball != null)
         {
             float t = Mathf.InverseLerp(minPower, maxPower, ball.GetPerfectShotRequiredPower());
-            Debug.Log(t);
             uiManager.UpdatePerfectZone(t, error);
             uiManager.UpdateBackboardZone(0.8f, error);
 
@@ -34,26 +38,20 @@ public class PlayerManager : MonoBehaviour
 
     protected virtual void OnEnable()
     {
-        //GestureSwipe.onSwipeStart += SwipeStart;
-        GestureSwipe.onSwipeUpdate += SwipeUpdate;
-        GestureSwipe.onSwipeEnd += SwipeEnd;
+        InputManager.onSwipeUpdate += SwipeUpdate;
+        InputManager.onSwipeEnd += SwipeEnd;
     }
 
     protected virtual void OnDisable()
     {
-        //GestureSwipe.onSwipeStart -= SwipeStart;
-        GestureSwipe.onSwipeUpdate -= SwipeUpdate;
-        GestureSwipe.onSwipeEnd -= SwipeEnd;
+        InputManager.onSwipeUpdate -= SwipeUpdate;
+        InputManager.onSwipeEnd -= SwipeEnd;
     }
-
-    //private void SwipeStart(Vector2 position)
-    //{
-        
-    //}
 
     private void SwipeUpdate(Vector2 startPosition, Vector2 endPosition)
     {
-        float distance = endPosition.y - startPosition.y;
+
+        float distance = (endPosition.y - startPosition.y) * inputMultiplier;
         distance = Mathf.Clamp(distance, 0.0f, maxSwipeDistance);
         float t = Mathf.InverseLerp(0f, maxSwipeDistance, distance);
 
@@ -64,7 +62,8 @@ public class PlayerManager : MonoBehaviour
 
     private void SwipeEnd(Vector2 startPosition, Vector2 endPosition)
     {
-        float distance = endPosition.y - startPosition.y;
+        
+        float distance = (endPosition.y - startPosition.y) * inputMultiplier;
         distance = Mathf.Clamp(distance, 0f, maxSwipeDistance);
         float t = Mathf.InverseLerp(0f, maxSwipeDistance, distance);
 
@@ -72,8 +71,6 @@ public class PlayerManager : MonoBehaviour
         float remappedValue = Utilities.RemapValue(t, perfectT, 0.8f, error);
 
         ball.Shoot(Mathf.Lerp(minPower, maxPower, remappedValue));
-        Debug.Log("raw: " + t);
-        Debug.Log("remapped: " + remappedValue);
     }
 
     
