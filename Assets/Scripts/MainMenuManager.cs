5316c35f7619702a9110f1ac94995eda1a341923
CriCer <c.cerati04@gmail.com>
2026-02-26 23:16:12 +0100

    Removed vsync and set target framerate to 60

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Scripts : MonoBehaviour
 {
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
